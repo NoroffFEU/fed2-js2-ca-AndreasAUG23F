f90271802510a13f96dfa6a4ff17d89deb2e7431
AndreasAUG23F <andabr00198@stud.noroff.no>
2024-09-11 18:21:37 +0200

    login
    Made the login page

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,1 +1,29 @@
-export async function login({ email, password }) {}
+import { API_AUTH_LOGIN } from "../constants"; // Sett riktig API URL for login
+import { headers } from "../headers";
+
+export async function login({ email, password }) {
+  const body = {
+    email: email,
+    password: password,
+  };
+
+  try {
+    console.log("trying to login");
+    const response = await fetch(API_AUTH_LOGIN, {
+      method: "POST",
+      headers: headers(),
+      body: JSON.stringify(body),
+    });
+
+    if (response.ok) {
+      alert("User logged in");
+      window.location.href = "/";
+      const data = await response.json();
+      localStorage.setItem("token", JSON.stringify(data.data.accessToken));
+      console.log(data);
+    }
+  } catch (error) {
+    alert("An error occurred");
+    console.error(error);
+  }
+}
